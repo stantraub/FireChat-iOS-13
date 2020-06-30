dc1c1e330bc71162be268544e97fa595f68fac39
Stanley <stanley.traub@gmail.com>
2020-06-30 12:50:16 -0700

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Stanley Traub on 6/30/20.
+//  Copyright © 2020 Stanley Traub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
