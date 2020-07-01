40876defd8c64964d25e588a5c4bac916a59162b
Stanley <stanley.traub@gmail.com>
2020-06-30 20:52:28 -0700

    Second commit

---
+++ FireChat/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationController.swift
+//  FireChat
+//
+//  Created by Stanley Traub on 6/30/20.
+//  Copyright © 2020 Stanley Traub. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
