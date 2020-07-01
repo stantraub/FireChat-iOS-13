cdb4c79d0d0bd2d3be2f350fe6e77c7917215f7e
Stanley <stanley.traub@gmail.com>
2020-06-30 20:46:42 -0700

    Initial commit

---
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginController.swift
+//  FireChat
+//
+//  Created by Stanley Traub on 6/30/20.
+//  Copyright © 2020 Stanley Traub. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    //MARK: - Properties
+    private let iconImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "bubble.right")
+        iv.tintColor = .white
+        return iv
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Helpers
+    
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        view.backgroundColor = .systemPurple
+        
+        configureGradientLayer()
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 120, width: 120)
+    }
+    
+    func configureGradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
